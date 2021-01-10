708604e1cc6fef260a5f9d5268bcc6dcc5738af5
efiVanso <efi.vanso@gmail.com>
2021-01-10 16:29:40 +0200

    Finish navigation

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 
-export default function TrackListScreen() {
+export default function TrackListScreen({navigation}) {
     return (
         <View>
             <Text>TrackListScreen</Text>
+            <Button
+            title="Go to track details"
+            onPress={()=>{ navigation.navigate('TrackDetail')}}
+            />
         </View>
     )
 }
