4113fb27634b047b1835a940f799f6961c34086b
efiVanso <efi.vanso@gmail.com>
2021-01-14 11:44:44 +0200

    add Spacer - Helper Styling Components

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import { View, Text,StyleSheet, Button } from 'react-native'
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
 
-export default function SignUpScreen({ navigation}) {
-    return (
-        <View>
-            <Text>SignUpScreen</Text>
-            <Button
-            title=" Go to Signin"
-            onPress={()=> navigation.navigate('Signin')}
-            />
-            <Button
-            title=" Go to main flow"
-            onPress={()=> navigation.navigate('mainFlow')}
-            />
-        </View>
-    )
+export default function SignUpScreen({ navigation }) {
+  return (
+    <View>
+      <Spacer>
+        <Text h3> Sign Up for Tracker</Text>
+      </Spacer>
+      <Input label="Email" />
+      <Spacer />
+      <Input label="Password" />
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
+    </View>
+  );
 }
-const styles = StyleSheet.create({
-    
-})
+const styles = StyleSheet.create({});
