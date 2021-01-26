0d6fb729ca2380caf0efe94ea2e9f31f964a72ad
efiVanso <efi.vanso@gmail.com>
2021-01-26 20:30:21 +0200

    Sign in Screen
    Real Component Reuse!

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import AuthForm from '../components/AuthForm'
+import NavLink from '../components/NavLink'
 
 export default function SignInScreen() {
     return (
-        <View>
-            <Text>SignInScreen</Text>
+        <View style={styles.container}>
+            <AuthForm
+            headerText="Sign In to Your Account"
+            errorMessage=""
+            onSubmit={()=>{}}
+            submitButtonText="Sign In"
+            />
+            <NavLink text="Don't have an account? Sign up instead"
+            routeName="Signup"
+            />
         </View>
     )
 }
+SignInScreen.navigationOptions = () => {
+    return {
+      headerShown: false,
+    };
+  };
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      marginBottom: 200,
+    },
+    
+  });
