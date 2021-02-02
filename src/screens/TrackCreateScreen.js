2f751ff2d95029559a0892ca78ad433b95c7e487
efiVanso <efi.vanso@gmail.com>
2021-02-02 19:09:25 +0200

    Extracting Logic to a Hook,

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,39 +1,16 @@
 import '../_mockLocation'// fake location
-import React, {useEffect, useState, useContext} from 'react'
+import React, { useContext} from 'react'
 import { View, StyleSheet } from 'react-native'
 import Map from '../components/Map'
 import { Text } from 'react-native-elements'
 import { SafeAreaView } from 'react-navigation'
-import {requestPermissionsAsync, watchPositionAsync, Accuracy} from 'expo-location'
 import { Context as LocationContext}  from '../context/LocationContext'
+import useLocation from '../hooks/useLocation'
 
 export default function TrackCreateScreen() {
     const {addLocation} = useContext(LocationContext)
-    const [err, setErr] = useState(null)
-
-    const startWatching = async () => {
-        try {
-          const { granted } = await requestPermissionsAsync();
-          if (!granted) {
-            throw new Error('Location permission not granted');
-          }
-          await watchPositionAsync({
-              accuracy: Accuracy.BestForNavigation,
-              timeInterval: 1000, // update location every 1 second
-              distanceInterval: 10 //or every 10 meters 
-          }, (location)=>{
-              addLocation(location)
-              //console.log(location)
-          })
-        } catch (e) {
-          setErr(e);
-        }
-      };
-
-useEffect(()=>{
-    startWatching();
-},[]);
-
+    const [err] = useLocation(addLocation);
+    
     return (
         <SafeAreaView forceInset={{top: 'always'}}>
             <Text h2>create a Track</Text>
